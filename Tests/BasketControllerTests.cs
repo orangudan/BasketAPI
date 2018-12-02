d0c83748cc1bf9e24a4b13a6d734aa300d6d3478
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-02 13:47:56 +0000

    Share setup in basket controller tests

--- Tests/BasketControllerTests.cs
+++ Tests/BasketControllerTests.cs
@@ -14,24 +14,30 @@ namespace Tests
         private const string FoundBasketId = "f5b8861f-73e6-4624-b37d-8b8b5b93a229";
         private const string NotFoundBasketId = "6c696970-4c89-46bd-9b89-1fc2c27ef71e";
 
-        [Test]
-        public void get_returns_not_found_if_basket_missing()
-        {
-            var controller = new BasketController(new InMemoryBasketQuery(), new InMemoryBasketAdder(Guid.Parse(TestId)));
-            var result = controller.Get(Guid.Parse(NotFoundBasketId));
-            Assert.That(result, Is.TypeOf<NotFoundResult>());
-        }
+        private BasketController _controller;
 
-        [Test]
-        public void get_returns_basket_if_basket_exists()
+        [SetUp]
+        public void Set_up_controller()
         {
             var baskets = new List<Basket>
             {
                 new Basket {Id = Guid.Parse(FoundBasketId)}
             };
 
-            var controller = new BasketController(new InMemoryBasketQuery(baskets), new InMemoryBasketAdder(Guid.Parse(TestId)));
-            var result = controller.Get(Guid.Parse(FoundBasketId));
+            _controller =  new BasketController(new InMemoryBasketQuery(baskets), new InMemoryBasketAdder(Guid.Parse(TestId)));
+        }
+
+        [Test]
+        public void get_returns_not_found_if_basket_missing()
+        {
+            var result = _controller.Get(Guid.Parse(NotFoundBasketId));
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void get_returns_basket_if_basket_exists()
+        {
+            var result = _controller.Get(Guid.Parse(FoundBasketId));
             Assert.That(result, Is.TypeOf<OkObjectResult>());
             Assert.That(((OkObjectResult)result).Value, Is.TypeOf<Basket>());
         }
@@ -39,16 +45,14 @@ namespace Tests
         [Test]
         public void result_is_ok_when_basket_created()
         {
-            var controller = new BasketController(new InMemoryBasketQuery(), new InMemoryBasketAdder(Guid.Parse(TestId)));
-            var result = controller.Post();
+            var result = _controller.Post();
             Assert.That(result, Is.TypeOf<OkObjectResult>());
         }
 
         [Test]
         public void result_contains_newly_created_basket()
         {
-            var controller = new BasketController(new InMemoryBasketQuery(), new InMemoryBasketAdder(Guid.Parse(TestId)));
-            var result = controller.Post();
+            var result = _controller.Post();
             Assert.That(result, Is.TypeOf<OkObjectResult>());
             Assert.That(((OkObjectResult)result).Value, Is.TypeOf<Basket>());
         }
@@ -56,8 +60,7 @@ namespace Tests
         [Test]
         public void result_contains_basket_id()
         {
-            var controller = new BasketController(new InMemoryBasketQuery(), new InMemoryBasketAdder(Guid.Parse(TestId)));
-            var result = controller.Post();
+            var result = _controller.Post();
             var basket = (Basket)((OkObjectResult)result).Value;
             Assert.That(basket.Id, Is.EqualTo(Guid.Parse(TestId)));
         }
