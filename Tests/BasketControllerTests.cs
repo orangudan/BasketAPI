6defa18b4a40840792eca795e7064b5b67497292
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-02 13:50:10 +0000

    Reduce Guid.Parsing in basket controller tests

--- Tests/BasketControllerTests.cs
+++ Tests/BasketControllerTests.cs
@@ -10,9 +10,9 @@ namespace Tests
     [TestFixture]
     public class BasketControllerTests
     {
-        private const string TestId = "ab0d1822-06d8-4a63-8761-3f5ac7774671";
-        private const string FoundBasketId = "f5b8861f-73e6-4624-b37d-8b8b5b93a229";
-        private const string NotFoundBasketId = "6c696970-4c89-46bd-9b89-1fc2c27ef71e";
+        private readonly Guid NewlyCreatedBasketId = Guid.Parse("ab0d1822-06d8-4a63-8761-3f5ac7774671");
+        private readonly Guid FoundBasketId = Guid.Parse("f5b8861f-73e6-4624-b37d-8b8b5b93a229");
+        private readonly Guid NotFoundBasketId = Guid.Parse("6c696970-4c89-46bd-9b89-1fc2c27ef71e");
 
         private BasketController _controller;
 
@@ -21,23 +21,23 @@ namespace Tests
         {
             var baskets = new List<Basket>
             {
-                new Basket {Id = Guid.Parse(FoundBasketId)}
+                new Basket {Id = FoundBasketId}
             };
 
-            _controller =  new BasketController(new InMemoryBasketQuery(baskets), new InMemoryBasketAdder(Guid.Parse(TestId)));
+            _controller =  new BasketController(new InMemoryBasketQuery(baskets), new InMemoryBasketAdder(NewlyCreatedBasketId));
         }
 
         [Test]
         public void get_returns_not_found_if_basket_missing()
         {
-            var result = _controller.Get(Guid.Parse(NotFoundBasketId));
+            var result = _controller.Get(NotFoundBasketId);
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
         [Test]
         public void get_returns_basket_if_basket_exists()
         {
-            var result = _controller.Get(Guid.Parse(FoundBasketId));
+            var result = _controller.Get(FoundBasketId);
             Assert.That(result, Is.TypeOf<OkObjectResult>());
             Assert.That(((OkObjectResult)result).Value, Is.TypeOf<Basket>());
         }
@@ -62,7 +62,7 @@ namespace Tests
         {
             var result = _controller.Post();
             var basket = (Basket)((OkObjectResult)result).Value;
-            Assert.That(basket.Id, Is.EqualTo(Guid.Parse(TestId)));
+            Assert.That(basket.Id, Is.EqualTo(NewlyCreatedBasketId));
         }
     }
 
