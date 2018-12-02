4e42aa02d156aa007be734471f77017633a82649
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-02 20:14:44 +0000

    Remove unused field, rename fields

--- Tests/BasketItemControllerTests.cs
+++ Tests/BasketItemControllerTests.cs
@@ -14,44 +14,37 @@ namespace Tests
     public class BasketItemControllerTests
     {
         private readonly Guid NewlyAddedProductId = Guid.Parse("eb29e491-f786-4d63-b09d-40956043c542");
+
         private readonly Guid FoundProductId = Guid.Parse("3fa180b2-e65b-48ee-9e31-a032dec504b1");
-        private readonly Guid NotFoundProductId = Guid.Parse("4bf673a3-cf0f-4f79-b9ac-ef922a1ddde8");
-        private readonly Guid NotFoundBasketId = Guid.Parse("6c696970-4c89-46bd-9b89-1fc2c27ef71e");
+        private readonly Guid MissingProductId = Guid.Parse("4bf673a3-cf0f-4f79-b9ac-ef922a1ddde8");
+
         private readonly Guid FoundBasketId = Guid.Parse("f5b8861f-73e6-4624-b37d-8b8b5b93a229");
-        private readonly Guid NewBasketItemId = Guid.Parse("8aeb643d-37a3-420f-ac30-9181148a7cb5");
+        private readonly Guid MissingBasketId = Guid.Parse("6c696970-4c89-46bd-9b89-1fc2c27ef71e");
 
         private BasketItemController _controller;
 
         [SetUp]
         public void Set_up_controller()
         {
-            var baskets = new List<Basket>
-            {
-                new Basket
-                {
-                    Id = FoundBasketId,
-                    Items = new List<BasketItem>
-                    {
-                        new BasketItem { ProductId = FoundProductId }
-                    }
-                }
-            };
+            var repository = new InMemoryBasketRepository(FoundBasketId, new List<Basket>());
+            var basket = repository.Add();
+            basket.AddItem(FoundProductId, 1);
 
             _controller = new BasketItemController(
-                new InMemoryBasketRepository(Guid.NewGuid(), baskets));
+                repository);
         }
 
         [Test]
         public void get_returns_not_found_if_basket_missing()
         {
-            var result = _controller.Get(NotFoundBasketId, FoundProductId);
+            var result = _controller.Get(MissingBasketId, FoundProductId);
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
         [Test]
         public void get_returns_not_found_if_basket_item_missing()
         {
-            var result = _controller.Get(FoundBasketId, NotFoundProductId);
+            var result = _controller.Get(FoundBasketId, MissingProductId);
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
@@ -66,7 +59,7 @@ namespace Tests
         [Test]
         public void post_returns_not_found_if_basket_missing()
         {
-            var result = _controller.Post(NotFoundBasketId, new AddBasketItem { ProductId = NewlyAddedProductId, Quantity = 3 });
+            var result = _controller.Post(MissingBasketId, new AddBasketItem { ProductId = NewlyAddedProductId, Quantity = 3 });
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
@@ -85,14 +78,14 @@ namespace Tests
         [Test]
         public void update_returns_not_found_if_basket_missing()
         {
-            var result = _controller.Update(NotFoundBasketId, FoundProductId, new UpdateBasketItem { Quantity = 5 });
+            var result = _controller.Update(MissingBasketId, FoundProductId, new UpdateBasketItem { Quantity = 5 });
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
         [Test]
         public void update_returns_not_found_if_basket_item_missing()
         {
-            var result = _controller.Update(FoundBasketId, NotFoundProductId, new UpdateBasketItem { Quantity = 5 });
+            var result = _controller.Update(FoundBasketId, MissingProductId, new UpdateBasketItem { Quantity = 5 });
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
@@ -109,14 +102,14 @@ namespace Tests
         [Test]
         public void delete_returns_not_found_if_basket_missing()
         {
-            var result = _controller.Delete(NotFoundBasketId, FoundProductId);
+            var result = _controller.Delete(MissingBasketId, FoundProductId);
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
         [Test]
         public void delete_returns_not_found_if_basket_item_missing()
         {
-            var result = _controller.Delete(FoundBasketId, NotFoundProductId);
+            var result = _controller.Delete(FoundBasketId, MissingProductId);
             Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
 
