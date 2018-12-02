c11ae9fad8ff0ffaeadfcd53ca11ab94c7bb18e5
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-02 11:05:34 +0000

    Reduce duplication in basket tests

--- Tests/BasketControllerTests.cs
+++ Tests/BasketControllerTests.cs
@@ -8,14 +8,13 @@ namespace Tests
     [TestFixture]
     public class BasketControllerTests
     {
+        private const string TestId = "ab0d1822-06d8-4a63-8761-3f5ac7774671";
+        private Func<Basket> _basketAdder = () => new Basket { Id = Guid.Parse(TestId) };
+
         [Test]
         public void result_is_ok_when_basket_created()
         {
-            var id = Guid.Parse("ab0d1822-06d8-4a63-8761-3f5ac7774671");
-
-            Func<Basket> basketAdder = () => new Basket { Id = id };
-
-            var controller = new BasketController(basketAdder);
+            var controller = new BasketController(_basketAdder);
             var result = controller.Post();
             Assert.That(result, Is.TypeOf<OkObjectResult>());
         }
@@ -23,11 +22,7 @@ namespace Tests
         [Test]
         public void result_contains_newly_created_basket()
         {
-            var id = Guid.Parse("ab0d1822-06d8-4a63-8761-3f5ac7774671");
-
-            Func<Basket> basketAdder = () => new Basket { Id = id };
-
-            var controller = new BasketController(basketAdder);
+            var controller = new BasketController(_basketAdder);
             var result = controller.Post();
             Assert.That(result, Is.TypeOf<OkObjectResult>());
             Assert.That(((OkObjectResult)result).Value, Is.TypeOf<Basket>());
@@ -36,15 +31,10 @@ namespace Tests
         [Test]
         public void result_contains_basket_id()
         {
-            var id = Guid.Parse("ab0d1822-06d8-4a63-8761-3f5ac7774671");
-
-            Func<Basket> basketAdder = () => new Basket { Id = id };
-
-            var controller = new BasketController(basketAdder);
+            var controller = new BasketController(_basketAdder);
             var result = controller.Post();
-
             var basket = (Basket)((OkObjectResult)result).Value;
-            Assert.That(basket.Id, Is.EqualTo(id));
+            Assert.That(basket.Id, Is.EqualTo(Guid.Parse(TestId)));
         }
     }
 }
