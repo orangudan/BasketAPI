5da73a879dfec4d9f628436c8c54f4681bdb1147
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-02 11:32:14 +0000

    Test that basket item controller returns not found if basket missing

---
+++ BasketAPI/Controllers/BasketItemController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketItemController : ControllerBase
+    {
+        private IEnumerable<Basket> _baskets;
+
+        public BasketItemController(IEnumerable<Basket> baskets)
+        {
+            _baskets = baskets;
+        }
+
+        [HttpPost("{basketId}")]
+        public ActionResult Post(Guid basketId)
+        {
+            var basket = _baskets.SingleOrDefault(b => b.Id == basketId);
+
+            if (basket == null)
+                return NotFound();
+
+            return Ok();
+        }
+    }
+}
---
+++ Tests/BasketItemControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using BasketAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestFixture]
+    public class BasketItemControllerTests
+    {
+        [Test]
+        public void result_is_not_found_when_basket_doesnt_exist()
+        {
+            IEnumerable<Basket> baskets = new List<Basket>
+            {
+                new Basket { Id = Guid.Parse("f5b8861f-73e6-4624-b37d-8b8b5b93a229")}
+            };
+
+            var controller = new BasketItemController(baskets);
+            var result = controller.Post(Guid.Parse("6c696970-4c89-46bd-9b89-1fc2c27ef71e"));
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
+        }
+    }
+}
