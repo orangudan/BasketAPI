f2807b429513cdb6a2f34f277c8e31473e864ed6
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-02 10:31:27 +0000

    Test that basket controller returns ok

---
+++ BasketAPI/Controllers/BasketController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasketAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        public ActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Tests/BasketControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using BasketAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class BasketControllerTests
+    {
+        [Test]
+        public void returns_success_when_basket_created()
+        {
+            var controller = new BasketController();
+            var result = controller.Post();
+            Assert.That(result, Is.TypeOf<OkResult>());
+        }
+    }
+}
